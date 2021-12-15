9f0208b49c499693270b06df9838fbf4f2d77468
linpeixu <1966353889@qq.com>
2021-12-15 11:36:28 +0800

    提交工程

--- customTask/src/main/java/com/cloudling/customtask/CustomTask.java
+++ customTask/src/main/java/com/cloudling/customtask/CustomTask.java
@@ -1,52 +1,5 @@
-# CustomTask
+package com.cloudling.customtask;
 
-Android开启定时任务通用的封装
-先看接入步骤：
-Step 1. Add the JitPack repository to your build file
-Add it in your root build.gradle at the end of repositories:
-```java
-    allprojects {
-		repositories {
-			...
-			maven { url 'https://jitpack.io' }
-		}
-	}
-```
-Step 2. Add the dependency
-```java
-    dependencies {
-                        implementation 'com.github.linpeixu:CustomTask:1.0.1'
-	        //或者implementation 'com.gitlab.linpeixu:customtask:1.0.1'
-	}
-```
-
-背景：
-
-最近接到一个需求，在特定条件下定时轮询接口查询相关信息，直到拿到想要的信息为止，为了方便以后使用，在这里做了一个封装，假设需要定时查询订单状态，先看使用方法：
-
-```java
-CustomTask<Long, Long> customTask = new CustomTask.Builder<Long, Long>()
-                .delay(3000)//延迟3秒执行，根据需要自行更改
-                .interval(5000)//轮询间隔5秒，根据需要自行更改
-                .task(new CheckOrderStateTask() {//具体的任务执行逻辑
-                    @Override
-                    public Long getSupport() {
-                       /*重写getSupport()返回CheckOrderStateTask执行需要用到的参数*/
-                        return getIntent().getLongExtra("id", 0);
-                    }
-                })
-                .callback(new CustomTask.OnResultListener<Long>() {
-                    @Override
-                    public void onComplete(Long aLong) {
-                       /*任务执行完成后的回调（此时应为拿到想要的结果）*/
-                        ToastUtil.getInstance().show(String.valueOf(aLong));
-                    }
-                }).build();
-        customTask.start();
-```
-CustomTask代码如下：
-
-```java
 import android.os.Handler;
 import android.os.Looper;
 
@@ -227,7 +180,7 @@ public class CustomTask<Support, Result> {
          */
         void cancel();
 
-      /**
+        /**
          * 销毁任务（一般在页面退出时调用）
          */
         void onDestroy();
@@ -252,73 +205,3 @@ public class CustomTask<Support, Result> {
         void process(boolean carryOn, Result result);
     }
 }
-```
-CheckOrderStateTask代码如下：
-
-```java
-public class CheckOrderStateTask implements CustomTask.SingleTask<Long, Long> {
-    /**
-     * 网络请求订阅存储对象
-    */
-    private CompositeDisposable mDisposables;
-    private final OrderApi orderApi;
-
-    public CheckOrderStateTask() {
-        mDisposables = new CompositeDisposable();
-        orderApi = ...
-    }
-
-
-    @Override
-    public void start(CustomTask.Scheduler<Long> scheduler) {
-        if (getSupport() != null) {
-            Disposable rx = RxUtils.rx(orderApi.checkOrderStatus(getSupport()), new OnNextOnError<Response<JsonObject>>() {
-                @Override
-                public void onError(Response response) {
-                    if (response != null) {
-                        if (response.status == -1) {
-                            /*订单已取消*/
-                            scheduler.process(false, -1L);
-                        }  else {
-                            scheduler.process(true, -1L);
-                        }
-                    }
-                }
-
-                @Override
-                public void onNext(Response<JsonObject> response) {
-                    long order_id = ...;
-                    scheduler.process(order_id <= 0, order_id);
-                }
-            });
-            mDisposables.add(rx);
-        } else {
-            scheduler.process(false, -1L);
-        }
-    }
-
-    @Override
-    public void cancel() {
-        if (mDisposables != null) {
-            mDisposables.dispose();
-        }
-    }
-
-
-    @Override
-    public void onDestroy() {
-        if (mDisposables != null) {
-            mDisposables.dispose();
-            mDisposables = null;
-        }
-    }
-
-    @Override
-    public Long getSupport() {
-        return null;
-    }
-
-
-}
-```
-这里的网络请求操作用到的是[Android快速集成网络库功能（rxJava+retrofit+okhttp）](https://blog.csdn.net/qq_33866343/article/details/106135963)，感兴趣的可以看看。
